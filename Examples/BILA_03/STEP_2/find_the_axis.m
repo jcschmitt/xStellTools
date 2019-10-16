aca809af4c020efb61be90991b0bf7a4ce5718b4
J.C. Schmitt <jcs0114@auburn.edu>
2019-10-16 17:13:14 -0500

    update

--- Examples/BILA_03/STEP_2/find_the_axis.m
+++ Examples/BILA_03/STEP_2/find_the_axis.m
@@ -1,14 +1,14 @@
-rGuess = 2.4;
+rGuess = 2.41;
 coilset = 'coilset_bila_03_stelsym';
 coil_current_array = 497292.;
 
 [rAxis, current_2p5_tesla] = find_magnetic_axis(coilset, coil_current_array, rGuess)
-
 % 
 % <----Finished axis search
-% <----Axis location: r = 2.3889
-% <----Coil current array for 2.5 T on-axis at phi=0 (Amps): 521815.4147
+% <----Axis location: r = 2.3881
+% <----Coil current array for 2.5 T on-axis at phi=0 (Amps): 519966.7677
 % rAxis =
-%      2.388919066924667e+00
+%      2.388121565178873e+00
 % current_2p5_tesla =
-%      5.218154146907073e+05
+%      5.199667677158224e+05
+%      
