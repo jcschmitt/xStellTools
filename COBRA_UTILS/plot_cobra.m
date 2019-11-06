09f689b6987670f751687c0182ef9a0246d1e84d
J.C. Schmitt <jcs0114@auburn.edu>
2019-11-06 16:29:46 -0600

    update

---
+++ COBRA_UTILS/plot_cobra.m
@@ -0,0 +1,24 @@
+function plot_cobra(cobra_extensions, title_prefix)
+%plot_cobra(cobra_extensions, title_prefix)
+% Plot COBRA output
+
+figure;box on; hold on; 
+for ii = 1:length(cobra_extensions)
+    cobradata{ii} = read_cobra(['cobra_grate.' cobra_extensions{ii}]);
+
+    [~,nsm1]=size(cobradata{ii}.grate);
+    ns = nsm1+1;
+    sall = linspace(0,1,ns);
+    splot = sall(2:end);
+    rhoplot=sqrt(splot);
+
+    %plot(rhoplot, cobradata{ii}.grate', 'o');
+    plot(rhoplot, cobradata{ii}.grate', '--', 'Linewidth', 3);
+    title(strrep( ([title_prefix, ' ', cobra_extensions{ii}]), '_', '\_'));
+    
+end
+xlabel('$\rho$', 'Interpreter', 'Latex')
+ylabel('\gamma\tau');
+grid on
+
+
---
+++ COBRA_UTILS/read_cobra.m
@@ -0,0 +1,55 @@
+function f = read_cobra(filename)
+%READ_COBRA(filename) Reads a COBRA output file into a COBRA structure.
+%
+%   READ_COBRA(filename) Reads a COBRA output file into a COBRA structure.
+%
+%   Example:
+%       cobra_data=read_cobra('grate_cobra.test');
+%
+%   Written by: Samuel Lazerson (lazerson@pppl.gov)
+%   Mior updates and fixes by J Schmitt
+%   Version:    1.5 (mod)
+%   Date:       09/28/2011, 2019
+
+fid=fopen(filename);
+data=fscanf(fid,'%f %f %d\n',3);
+zeta=data(1);
+theta=data(2);
+ns=data(3);
+%data=fscanf(fid,'%d %f\n',[2 ns]); % old way
+data=fscanf(fid,'%d %f %f\n',[3 ns]);
+surfs=data(1,:);
+grate=data(3,:);
+while ~feof(fid)
+    data=fscanf(fid,'%f %f %d\n',3);
+    if isempty(data); break; end
+    zeta=[zeta data(1)];
+    theta=[theta data(2)];
+    data=fscanf(fid,'%d %f %f\n',[3 ns]);
+    grate=[grate; data(3,:)];
+end
+fclose(fid);
+f.grate = grate;
+f.zeta = zeta;
+f.theta = theta;
+f.ns = ns;
+
+% Now parse into ns by theta by zeta arrays
+% grate=permute(grate,[2 1]);
+% i_axis=unique(theta);
+% j_axis=unique(zeta);
+% if (zeta(length(j_axis)+1)==zeta(length(j_axis)+2))
+%     j_axis=[j_axis 36];
+% end
+% f.grate=zeros(ns,length(i_axis),length(j_axis));
+% for k=1:size(grate,2)
+%     f.grate(:,i_axis==theta(k),j_axis==zeta(k))=grate(:,k);
+% end
+% 
+% f.zeta=zeta;
+% f.theta=theta;
+% f.ns=ns;
+% f.surfs=surfs;
+
+end
+
