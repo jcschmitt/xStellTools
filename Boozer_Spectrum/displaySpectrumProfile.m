de3a1a81b199526aa12d96643625b3a77d223cc6
J.C. Schmitt <jcs0114@auburn.edu>
2019-12-20 13:24:17 -0600

    added boozer display matlab funtion'

---
+++ Boozer_Spectrum/displaySpectrumProfile.m
@@ -0,0 +1,119 @@
+function [] = displaySpectrumProfile(folder, text_title, spectrumType, ...
+    surfs, ModesToDisplay, COLOR_SPEC, STYLE_SPEC, XAXIS, fig_handle_in)
+% function [] = displaySpectrumProfile(folder, text_title, spectrumType, ...
+%     surfs, ModesToDisplay, COLOR_SPEC, STYLE_SPEC, XAXIS, fig_handle_in)
+% folder: absolute or relative folderpath for the data files
+% text_title: The title string that will be added to the figure.
+% spectrumType: 'boozer' or 'hamada'  (only boozer has been tested so far)
+% surfs: the indices of the surfaces that are to be ploted.
+% the following are optional:
+%   ModesToDisplay: the indices of the modes to display (sorted by magnitude)
+%   COLOR_SPEC: the color of the lines/symbols
+%   STYLE_SPEC: the style of the plotted data
+%   XAXIS: The x-axis to be used, 
+%   fig_handle_in: For overplotting on the figure #(fig_handle_in);
+
+
+if nargin < 9
+    fig_handle_in = figure;
+end
+
+if nargin < 8
+    XAXIS = surfs;
+elseif length(XAXIS) ~= length(surfs)
+    error('<----Number of surfaces does not match specs of XAXIS');
+end
+if nargin < 7
+    STYLE_SPEC = '-';
+end
+
+if nargin < 6
+    COLOR_SPEC = 'b';
+end
+if (nargin < 5)
+    ModesToDisplay = 1:10;
+end
+if (nargin < 4)
+    error('<----Not enough inputs');
+end
+
+SHOW_LABELS = 1;
+
+disp(['<----Displaying radial profile of ' spectrumType ' spectrum in folder: ' folder]);
+
+numSurfs = length(surfs);
+
+for ii = 1:numSurfs
+    filename = ['SFL_SpectrumFollow_' spectrumType '_Data_surface_' num2str(surfs(ii)) '.mat'];
+    filenameComplete = [folder '/' filename];
+
+    load(filenameComplete, ...
+        'spectrumType', ...
+        'dV_dPsi', ...
+        'g_Boozer', ...
+        'pk_pos_sorted', ...
+        'iota_best', ...
+        'nm_amp', ...
+        'nm_avail', ...
+        'n_values', ...
+        'm_values');
+     nm_amp_all{ii}.nm_amp = nm_amp;
+    %nm_amp_all{ii}.nm_amp = nm_amp*2;  % for comparing 1/2 T to 1 T calculations
+    %disp('Mysterious factor of 2..Are you sure???')
+    iota_best_all(ii) = iota_best;
+    disp(['<----File # ' num2str(surfs(ii)) ' loaded']);
+end
+
+% Re-sort the amplitude data into nice arrays
+num_n = length(n_values);
+num_m = length(m_values);
+for ii = 1:num_n
+    for jj = 1:num_m
+        for kk = 1:numSurfs
+            spectrumAmp_versus_surfNum( (ii - 1) * num_m + jj, kk ) = nm_amp_all{kk}.nm_amp(ii, jj);
+
+        end
+        mode_label{ (ii - 1) * num_m + jj} = [ '(' num2str(n_values(ii)) ',' num2str(m_values(jj)) ')' ];
+    end
+end
+
+% all mode except (0,0) should be x2
+% spectrumAmp_versus_surfNum = 4* spectrumAmp_versus_surfNum;  % for when you want 1T spectrum from 0.5 T line follow data
+%spectrumAmp_versus_surfNum = 2* spectrumAmp_versus_surfNum; % for normal
+% display
+
+%m_zero = find(m_values == 0);
+%n_zero = find(n_values == 0);
+%spectrumAmp_versus_surfNum( (n_zero-1) * num_m + m_zero, :) = ...
+%    0.5 * spectrumAmp_versus_surfNum( (n_zero-1) * num_m + m_zero, :);
+
+
+for ii = 1:num_n*num_m
+    max_of_mode(ii) = max(abs(spectrumAmp_versus_surfNum(ii,:)));
+end
+
+[~, sorted_indices] = sort(max_of_mode, 'descend');
+
+figure(fig_handle_in)
+legend_text = [];
+for ii = ModesToDisplay
+    jj = mod(ii-1, length(COLOR_SPEC)) + 1;
+    plot(XAXIS, spectrumAmp_versus_surfNum(sorted_indices(ii), :), [COLOR_SPEC(jj) STYLE_SPEC(jj) '']);
+    hold on
+    if SHOW_LABELS
+        %text( XAXIS(end)+.01*(XAXIS(end) - XAXIS(1)), spectrumAmp_versus_surfNum(sorted_indices(ii), end), ...
+        %    mode_label(sorted_indices(ii)), 'Color', COLOR_SPEC(jj));
+        [XAXIS_max, ind_max] = max(XAXIS);
+        [XAXIS_min, ind_min] = min(XAXIS);
+        text( XAXIS_max+.01*(XAXIS_max - XAXIS_min), spectrumAmp_versus_surfNum(sorted_indices(ii), ind_max), ...
+            mode_label(sorted_indices(ii)), 'Color', COLOR_SPEC(jj));
+    end
+    legend_text = [legend_text ; mode_label(sorted_indices(ii))];
+end
+xlabel('Surf #');
+ylabel('Spectrum amplitude');
+cur_axis = axis;
+axis([cur_axis(1) cur_axis(2)*1.015 cur_axis(3) cur_axis(4)]);
+title(text_title);
+%title([folder ' ' spectrumType]);
+legend(legend_text);
--- Boozer_Spectrum/plot_Boozer_spectrum.m
+++ Boozer_Spectrum/plot_Boozer_spectrum.m
@@ -1,6 +1,6 @@
 function [fig_handle_1, fig_handle_2] = ...
     plot_Boozer_spectrum(Boozer_output_file, surface_to_plot, ...
-    modes_to_display, PLOT_SPECTRUM, PLOT_SURFACE_2D, ...
+    modes_to_display, sorting_surface, PLOT_SPECTRUM, PLOT_SURFACE_2D, ...
     color_list, symbol_cells, line_cells, colormap_in)
 % function [x_3D, y_3D, z_3D, modB_recon_3D] = ...
 %     plot_boozer_spectrum(Boozer_output_file, surface_to_plot, ...
@@ -28,7 +28,7 @@ function [fig_handle_1, fig_handle_2] = ...
 %======================================================================
 
 
-if nargin < 8
+if nargin < 10
     % color_list =  'bkgrcmybkgrcmy';
     % symbol_list = '...xosdxxx....';
     disp('<----Using default color/marker/line scheme.')
@@ -41,11 +41,14 @@ if nargin < 8
 end
 
 % These variable control the generation of figures
-if nargin < 5
+if nargin < 6
     PLOT_SURFACE_2D = 1;
 end
-if nargin < 4
+if nargin < 5
     PLOT_SPECTRUM = 1;
+end
+if nargin < 4
+    sorting_surface = 1;
 end
 
 % This controls the visibility (on/off) of mode #'s on the figures.
@@ -65,14 +68,21 @@ end
 
 boozer_data = read_Boozer_output(Boozer_output_file);
 ns_b = boozer_data.ns_b; % number of surfs
+nfp_b = boozer_data.nfp_b; % number of surfs
 disp(['<----Found ' num2str(ns_b) ' surfaces in ' Boozer_output_file]);
+
+lasym_b = boozer_data.lasym; % " " of total modes = (nboz_b * 2 + 1) * (mboz_b) - nboz_b
 
 mnboz_b = boozer_data.mnboz_b; % " " of total modes = (nboz_b * 2 + 1) * (mboz_b) - nboz_b
 ixm_b = boozer_data.xm_b; % poloidal mode numbers
 ixn_b = boozer_data.xn_b; % toroidal mode numbers
-bmnc_b = boozer_data.bmnc_b; % mode magnitudes (signed)
+bmnc_b = boozer_data.bmnc_b; % mode magnitudes (signed) stellarator symmetric terms
 phi_b = boozer_data.phi_b; % enclosed toroidal flux (as a function of radius)
 iota_b = boozer_data.iota_b; % boozer iota
+
+if lasym_b
+    bmns_b = boozer_data.bmns_b; % mode magnitudes (signed) stellarator symmetric terms
+end
 
 % [ixn_b ixm_b bmnc_b(1,:)'] would give list of tor, pol mode number and
 % magnitude for surface 1
@@ -81,35 +91,56 @@ iota_b = boozer_data.iota_b; % boozer iota
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Re-sort the amplitude data into nice arrays
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-max_max = -inf;
+max_max_stelsym = -inf;
+max_max_nonstelsym = -inf;
 
-max_of_mode = zeros(1,mnboz_b);
-mode_label = cell(1,mnboz_b);
+max_of_mode_stelsym = zeros(1,mnboz_b);
+mode_label_stelsym = cell(1,mnboz_b);
+mode_label_nonstelsym = cell(1,mnboz_b);
 
+% stellarator symmetric modes
 for ii = 1:mnboz_b
     % to use the 'global' max components
-    max_of_mode(ii) = max( abs( bmnc_b(:, ii) ));
-    % to use the 'surface' maximum components
+    max_of_mode_stelsym(ii) = max( abs( bmnc_b(:, ii) ));
+    if lasym_b
+        max_of_mode_nonstelsym(ii) = max( abs( bmns_b(:, ii) ));
+    end
+    % to use the 'surface' maximum components instead of the global max
     % max_of_mode(ii) = max( abs( bmnc_b(surface_to_plot, ii) ));
-    max_max = max(max_max, abs(bmnc_b(1,ii)));
+    % max_of_mode_stelsym(ii) = max( abs( bmnc_b(surface_to_plot, ii) ));
+    % max_of_mode_nonstelsym(ii) = max( abs( bmns_b(surface_to_plot, ii) ));
     
-    mode_label{ii} = [ '(' num2str(ixn_b(ii)) ',' num2str(ixm_b(ii)) ')' ];
+    % to use inner surface (I think)
+    % max_max = max(max_max, abs(bmnc_b(1,ii)));
+    max_max_stelsym = max(max_max_stelsym, abs(bmnc_b(1,ii)));
+    if lasym_b
+        max_max_nonstelsym = max(max_max_nonstelsym, abs(bmns_b(1,ii)));
+    end  
+    
+    mode_label_stelsym{ii} = [ '(' num2str(ixn_b(ii)) ',' num2str(ixm_b(ii)) ')' ];
+    mode_label_nonstelsym{ii} = [ '(' num2str(ixn_b(ii)) ',' num2str(ixm_b(ii)) ')' ];
     %if (ixn_b(ii) == 48 && ixm_b(ii) ==0)
     %    keyboard
     %end
 end
 
-[~, sorted_indices] = sort(max_of_mode, 'descend');
+if (max_max_nonstelsym > max_max_stelsym)
+    error('<----How did this happen?');
+end
 
+[~, sorted_indices_stelsym] = sort(max_of_mode_stelsym, 'descend');
+    if lasym_b
+
+[~, sorted_indices_nonstelsym] = sort(max_of_mode_nonstelsym, 'descend');
+    end
+    
 if PLOT_SPECTRUM
     % Make plots of the radial profile of the largest modes (sorted
     % according to the values of the mode on the 'surface_to_plot'
     if (modes_to_display ~= 0)
-        disp(['Displaying modes with indices: [' num2str(sorted_indices(modes_to_display)) '] ']);
-        disp(['Displaying modes with m nums:  [' num2str(ixm_b(sorted_indices(modes_to_display))') '] ']);
-        disp(['Displaying modes with n nums:  [' num2str(ixn_b(sorted_indices(modes_to_display))') '] ']);
-        fig_handle_1 = figure;box on;
-        legend_text = [];
+        disp(['Displaying modes with indices: [' num2str(sorted_indices_stelsym(modes_to_display)) '] ']);
+        disp(['Displaying modes with m nums:  [' num2str(ixm_b(sorted_indices_stelsym(modes_to_display))') '] ']);
+        disp(['Displaying modes with n nums:  [' num2str(ixn_b(sorted_indices_stelsym(modes_to_display))') '] ']);
         
         x_values = phi_b(1:end)/phi_b(end);
         if PLOT_VS_R
@@ -119,43 +150,89 @@ if PLOT_SPECTRUM
         else
             XLABEL = 's = \Psi/\Psi_{LCFS}';
         end
-        %     XAXIS = ns_b;
         
+        % stellarator symmetric components
+        fig_handle_1 = figure;box on;
+        legend_text = [];
+        
+        %     XAXIS = ns_b;
         for ii = modes_to_display
             jj = mod(ii-1, length(color_list)) + 1;
-            plot(x_values(2:end), bmnc_b(:, sorted_indices(ii)) / max_max, ...
+            plot(x_values(2:end), bmnc_b(:, sorted_indices_stelsym(ii)) / max_max_stelsym, ...
                 'Color', color_list(jj), ...
                 'Marker', symbol_cells{jj}, 'LineStyle', line_cells{jj});
             hold on;grid on
             
             if SHOW_LABELS
-                text( x_values(end)+.01*(x_values(end) - x_values(1)), bmnc_b(end, sorted_indices(ii)) / max_max, ...
-                    mode_label(sorted_indices(ii)), 'Color', color_list(jj));
+                text( x_values(end)+.01*(x_values(end) - x_values(1)), bmnc_b(end, sorted_indices_stelsym(ii)) / max_max_stelsym, ...
+                    mode_label_stelsym(sorted_indices_stelsym(ii)), 'Color', color_list(jj));
             end
-            legend_text = [legend_text ; mode_label(sorted_indices(ii))];
+            legend_text = [legend_text ; mode_label_stelsym(sorted_indices_stelsym(ii))];
         end
         
         xlabel(XLABEL);
-        ylabel('Spectrum amplitude');
+        ylabel('Symmetric Spectrum Amplitude');
+        
         cur_axis = axis;
         axis([cur_axis(1) cur_axis(2)*1.015 cur_axis(3) cur_axis(4)]);
         title(strrep([Boozer_output_file], '_', '\_'));
         legend(legend_text);
         
         
+        % non-stellarator symmetric components
+        if lasym_b
+            fig_handle_1b = figure;box on;
+            legend_text_b = [];
+            
+            for ii = modes_to_display
+                jj = mod(ii-1, length(color_list)) + 1;
+                plot(x_values(2:end), bmns_b(:, sorted_indices_nonstelsym(ii)) / max_max_stelsym, ...
+                    'Color', color_list(jj), ...
+                    'Marker', symbol_cells{jj}, 'LineStyle', line_cells{jj});
+                hold on;grid on
+                
+                if SHOW_LABELS
+                    text( x_values(end)+.01*(x_values(end) - x_values(1)), bmns_b(end, sorted_indices_nonstelsym(ii)) / max_max_stelsym, ...
+                        mode_label_nonstelsym(sorted_indices_nonstelsym(ii)), 'Color', color_list(jj));
+                end
+                legend_text_b = [legend_text_b ; mode_label_nonstelsym(sorted_indices_nonstelsym(ii))];
+            end
+            
+            xlabel(XLABEL);
+            ylabel('Non-Symm Spectrum Amplitude');
+            cur_axis = axis;
+            axis([cur_axis(1) cur_axis(2)*1.015 cur_axis(3) cur_axis(4)]);
+            title(strrep([Boozer_output_file], '_', '\_'));
+            legend(legend_text_b);
+        end
+        
         for ii = modes_to_display
-            disp(['Mode with index: [' num2str(sorted_indices(ii)) '] ']);
-            disp(['Mode with (n,m) =  (' num2str(ixn_b(sorted_indices(ii))) ', ' ...
-                num2str(ixm_b(sorted_indices(ii))') ') ']);
+            disp(['ii = ' num2str(ii)])
+            disp(['StelSym Mode with index: [' num2str(sorted_indices_stelsym(ii)) '] ']);
+            disp(['StelSym Mode with (n,m) =  (' num2str(ixn_b(sorted_indices_stelsym(ii))) ', ' ...
+                num2str(ixm_b(sorted_indices_stelsym(ii))') ') ']);
             for jj = surface_to_plot
                 disp([' xaxis val = ' num2str(x_values(jj))]);
-                disp([' b_nm = ' num2str(bmnc_b(jj, sorted_indices(ii))) ]);
+                disp([' b_nmc = ' num2str(bmnc_b(jj, sorted_indices_stelsym(ii))) ]);
             end
         end
+        if lasym_b
+                for ii = modes_to_display
+                disp(['ii = ' num2str(ii)])
+                disp(['NonStelSym Mode with index: [' num2str(sorted_indices_nonstelsym(ii)) '] ']);
+                disp(['NonStelSym Mode with (n,m) =  (' num2str(ixn_b(sorted_indices_nonstelsym(ii))) ', ' ...
+                    num2str(ixm_b(sorted_indices_nonstelsym(ii))') ') ']);
+                for jj = surface_to_plot
+                    disp([' xaxis val = ' num2str(x_values(jj))]);
+                    disp([' b_nms = ' num2str(bmns_b(jj, sorted_indices_nonstelsym(ii))) ]);
+                end
+            end
+        end
+        
     end
 end
 try
-    make_my_plot_pretty3;
+%    make_my_plot_pretty3;
 catch
 end
 
@@ -165,52 +242,67 @@ if PLOT_SURFACE_2D
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     num_divisions = 400;
     num_contour_divisions = 10;
-    tor_angle = linspace(0,2*pi, num_divisions);
+    %tor_angle = linspace(0,2*pi, num_divisions);
+    %pol_angle = linspace(0,2*pi, num_divisions);
+    tor_angle = linspace(0,2*pi/nfp_b, num_divisions);
     pol_angle = linspace(0,2*pi, num_divisions);
     
     [tor_matrix, pol_matrix] = meshgrid(tor_angle, pol_angle);
     
     modB_recon_3D = zeros(size(pol_matrix));
     
-    for ii = (modes_to_display)
-        angle_value = (ixm_b(sorted_indices(ii)) * pol_matrix - ixn_b(sorted_indices(ii)) * tor_matrix);
-        modB_recon_3D = modB_recon_3D + bmnc_b(surface_to_plot, sorted_indices(ii)) * cos(angle_value);
+    if ~lasym_b
+        for ii = (modes_to_display)
+            angle_value = (ixm_b(sorted_indices_stelsym(ii)) * pol_matrix - ixn_b(sorted_indices_stelsym(ii)) * tor_matrix);
+            modB_recon_3D = modB_recon_3D + bmnc_b(surface_to_plot, sorted_indices_stelsym(ii)) * cos(angle_value);
+        end
+    else
+        for ii = (modes_to_display)
+            angle_value = (ixm_b(sorted_indices_stelsym(ii)) * pol_matrix - ixn_b(sorted_indices_stelsym(ii)) * tor_matrix);
+            modB_recon_3D = modB_recon_3D + ...
+                bmnc_b(surface_to_plot, sorted_indices_stelsym(ii)) * ...
+                cos(angle_value) + ...
+                bmns_b(surface_to_plot, sorted_indices_nonstelsym(ii)) * ...
+                sin(angle_value);
+        end
+        
     end
     
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % this section sets up the B- lines for the contour plot
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    tor_angle = linspace(0,2*pi, num_divisions);
-    pol_angle = linspace(0,2*pi, num_divisions);
-    
-    num_field_lines = 9;
-    B_0 = -2*pi; B_end = 2*pi;
-    BLine_0 = linspace(B_0, B_end, num_field_lines);
-    
-    % BLine_0 = 0;
-    BLines_tor = tor_angle;
-    BLines_pol = zeros(num_field_lines, num_divisions);
-    
-    for ii = 1:length(BLine_0)
-        BLines_pol(ii,:) = iota_b(surface_to_plot) * (BLines_tor - BLine_0(ii));
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % this section sets up the B- lines for the contour plot
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        tor_angle = linspace(0,2*pi/double(nfp_b), num_divisions);
+        pol_angle = linspace(0,2*pi, num_divisions);
+        
+        num_field_lines = 9;
+        B_0 = -2*pi; B_end = 2*pi;
+        BLine_0 = linspace(B_0, B_end, num_field_lines);
+        
+        % BLine_0 = 0;
+        BLines_tor = tor_angle;
+        BLines_pol = zeros(num_field_lines, num_divisions);
+        
+        for ii = 1:length(BLine_0)
+            BLines_pol(ii,:) = iota_b(surface_to_plot) * (BLines_tor - BLine_0(ii));
+        end
+        
+        fig_handle_2 = figure;
+        contourf(tor_angle, pol_angle, modB_recon_3D, num_contour_divisions);
+        colorbar;
+        colormap(colormap_in);
+        hold on;box on;
+        for ii = 1:length(BLine_0)
+            plot3(BLines_tor, BLines_pol(ii, :),2*ones(size(BLines_tor)), 'k:', 'LineWidth', 2);
+        end
+        xlabel('\zeta_{Boozer}','FontSize', 14);
+        ylabel('\theta_{Boozer}','FontSize', 14);
+        
+        view(2)
+        set(gca,'FontSize', 14);
+        title(['|B| on surf \rho = ' num2str(sqrt(phi_b(surface_to_plot)/ phi_b(end)))])
+        hold on;
+        axis([0 2*pi/nfp_b 0 2*pi]);
     end
     
-    fig_handle_2 = figure;
-    contourf(tor_angle, pol_angle, modB_recon_3D, num_contour_divisions);
-    colorbar;
-    colormap(colormap_in);
-    hold on;box on;
-    for ii = 1:length(BLine_0)
-        plot3(BLines_tor, BLines_pol(ii, :),2*ones(size(BLines_tor)), 'k:', 'LineWidth', 2);
-    end
-    xlabel('\zeta_{Boozer}','FontSize', 14);
-    ylabel('\theta_{Boozer}','FontSize', 14);
     
-    view(2)
-    set(gca,'FontSize', 14);
-    title(['|B| on surf \rho = ' num2str(sqrt(phi_b(surface_to_plot)/ phi_b(end)))])
-    hold on;
-    axis([0 2*pi 0 2*pi]);
-end
-
-
