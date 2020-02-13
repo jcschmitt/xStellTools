6c7ac34d2a348f24e4e0445de3e3190ccc4c365c
J.C. Schmitt <jcs0114@auburn.edu>
2020-02-13 13:41:45 -0600

    update

---
+++ Other/save_gcf_local.m
@@ -0,0 +1,24 @@
+function save_gcf_local(prefix)
+
+fh = gcf();
+%hgexport(fh, [prefix '.eps']);
+
+if exist([prefix '.fig'])
+    disp (['K====' prefix '.fig file exists.']);
+    prompt = ['K===Overwrite this file and others (.eps, .pdf, /jpg, .png) Y/n? (default = Y):'];
+    str = input(prompt, 's');
+    if isempty(str)
+        str = 'Y';
+    end
+end
+
+if str == 'Y'
+    savefig([prefix '.fig'])
+    print([prefix '.eps'], '-deps')
+    print([prefix '.pdf'], '-dpdf')
+    print([prefix '.jpg'], '-djpeg')
+    print([prefix '.png'], '-dpng')
+else
+    disp('<----Not saving files');
+end
+
